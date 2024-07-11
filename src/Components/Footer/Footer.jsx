f73776b9ffcdb4788e6b032c6b7ee3e85e480085
Dileep2109 <154053045+Dileep2109@users.noreply.github.com>
2024-07-11 20:30:44 +0530

    Admin

---
+++ src/Components/Footer/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Footer.css'
+const Footer = () => {
+  return (
+    <div className='footer'>
+      <hr />
+      <p>Email: smdileep777@gmail.com</p>
+      
+    </div>
+  )
+}
+
+export default Footer
